ec59a13834d27292e4f1568da40255e14f34dc01
luanborrges <86614372+luanborrges@users.noreply.github.com>
2021-08-21 14:18:50 -0300

    commit inicial

---
+++ android/app/src/main/kotlin/com/example/final_project_mobile2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.final_project_mobile2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/views.dart
@@ -0,0 +1,2 @@
+export 'package:final_project_mobile2/view/home.dart';
+export 'package:final_project_mobile2/app/my_app.dart';
